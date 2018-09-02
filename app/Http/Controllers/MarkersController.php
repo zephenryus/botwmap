078c669c2158265604e9b21792cd337e597789ee
Kevin Jensen <kevin@turtlerockweb.com>
2018-09-01 19:10:58 -0600

    added location markers resource

---
+++ app/Http/Controllers/MarkersController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Marker;
+use Illuminate\Http\Request;
+
+class MarkersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Marker $location
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Marker $location)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Marker $location
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Marker $location)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Marker              $location
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update (Request $request, Marker $location)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Marker  $location
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Marker $location)
+    {
+        //
+    }
+}
